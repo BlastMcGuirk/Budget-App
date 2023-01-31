0358560ed066a4d553fe6b2134bfbe69dd64f181
Brendan McGuirk <bwm1002@wildcats.unh.edu>
2023-01-30 19:43:12 -0500

    Fixed scrolling issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -109,7 +109,6 @@ export default function Home(props: Props) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
     },
